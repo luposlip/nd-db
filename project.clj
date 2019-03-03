6c58c8e670477e5c88fb49f3bd8f27a70357ad68
Henrik Mohr <luposlip@gmail.com>
2019-03-03 16:34:36 +0100

    Bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luposlip/ndjson-db "0.1.0"
+(defproject luposlip/ndjson-db "0.1.1"
   :description "Clojure library for using (huge) .ndjson files as lightning fast databases"
   :url "https://github.com/luposlip/ndjson-db"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
