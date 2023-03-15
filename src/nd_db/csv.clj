93d78037fde60d6cf642e9d62576d396748d03e0
Henrik Mohr <luposlip@gmail.com>
2023-03-15 13:43:49 +0100

    csv-row->data now closes over the header

--- src/nd_db/csv.clj
+++ src/nd_db/csv.clj
@@ -1,13 +1,27 @@
 (ns nd-db.csv
-  (:require [clojure.string :as s]))
+  (:require [clojure
+             [string :as s]
+             [edn :as edn]]))
 
-(defn csv-row->data [& {:keys [cols separator drop-n skip-last? col-parser]
-                        :or {drop-n 0 col-parser identity}}]
-  (fn [row]
-    (zipmap cols
-            (->> (s/split row (re-pattern (or separator ",")))
-                 (drop drop-n)
-                 (#(if skip-last?
-                     (butlast %)
-                     %))
-                 (map col-parser)))))
+(defn csv-row->data
+  "Closes over the header, returning a function that takes a row and
+   returns a resulting map with keyword keys from header.
+
+   optional param :col-parser is a function that can be used to parse
+   individual columns."
+  [& {:keys [header separator col-parser drop-n skip-last?]
+      :or {drop-n 0
+           col-parser identity}}]
+  {:pre [(string? header)]}
+  (let [cols (->> header
+                  (#(s/split % (re-pattern (or separator ","))))
+                  (map (comp keyword s/lower-case edn/read-string)))]
+    (fn [row]
+      {:pre [(string? row)]}
+      (zipmap cols
+              (->> (s/split row (re-pattern (or separator ",")))
+                   (drop drop-n)
+                   (#(if skip-last?
+                       (butlast %)
+                       %))
+                   (map col-parser))))))
