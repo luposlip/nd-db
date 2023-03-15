50a3a7d2291426eb716daeec0173acc1164055a2
Henrik Mohr <luposlip@gmail.com>
2023-03-15 13:34:28 +0100

    Preliminary work on #11

--- src/nd_db/csv.clj
+++ src/nd_db/csv.clj
@@ -1,1 +1,13 @@
-(ns nd-db.csv)
+(ns nd-db.csv
+  (:require [clojure.string :as s]))
+
+(defn csv-row->data [& {:keys [cols separator drop-n skip-last? col-parser]
+                        :or {drop-n 0 col-parser identity}}]
+  (fn [row]
+    (zipmap cols
+            (->> (s/split row (re-pattern (or separator ",")))
+                 (drop drop-n)
+                 (#(if skip-last?
+                     (butlast %)
+                     %))
+                 (map col-parser)))))
