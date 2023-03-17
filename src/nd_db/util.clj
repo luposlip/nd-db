5ee26a66f6a8258ccf18f06ea513925b6b487976
Henrik Mohr <luposlip@gmail.com>
2023-03-17 11:07:52 +0100

    Add fn checking if a string represents a number

--- src/nd_db/util.clj
+++ src/nd_db/util.clj
@@ -1,5 +1,14 @@
 (ns nd-db.util)
 
+(defn- num-chars* []
+  (->> (range 48 58)
+       (map char)
+       (cons \.)
+       (cons \,)
+       set))
+
+(def ^:private num-chars (memoize num-chars*))
+
 (defn str->hash [in]
   (.hashCode ^String in))
 
@@ -14,3 +23,25 @@
 
 (defn nippy-db? [candidate]
   (= :nippy (:doc-type @candidate)))
+
+(defn number-str?
+  "Takes a string, returns true if all chars are numeric.
+   Accepts punctuation (dot and comma).
+
+   Doesn't allow the number to begin with 0 followed by
+   another number (such as 008)."
+  [i]
+  {:pre [(string? i)]}
+  (loop [chars (seq i)
+         leading-zero? nil]
+    (if (seq chars)
+      (if-let [fc (num-chars (first chars))]
+        (cond (or (false? leading-zero?)
+                  (and (nil? leading-zero?)
+                       (not= fc \0)))
+              (recur (rest chars) false)
+              (and (nil? leading-zero?) (= fc \0))
+              (recur (rest chars) true)
+              :else false)
+        false)
+      true)))
